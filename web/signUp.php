2e9a883f8b23cdf0f9f74f2fb1d13eb4bcf0ea00
Jeff <rubikscubedude@gmail.com>
2021-07-03 13:51:14 -0700

    trying to fix the add an employee page still

--- web/signUp.php
+++ web/signUp.php
@@ -1,3 +1,44 @@
+<?php
+
+session_start();
+$badLogin = false;
+if (isset($_POST['employee_id']) && isset($_POST['employee_password']) && isset($_POST['employee_name'])
+{
+	$id = $_POST['employee_id'];
+	$password = $_POST['employee_password'];
+	$name = $_POST['employee_name'];
+	
+	require("dbConnect.php");
+	$db = get_db();
+	$query = 'INSERT INTO naf_employee VALUES (:id, :password, :name)';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':id', $id);
+	$statement->bindValue(':password', $password);
+	$statement->bindValue(':name', $name);
+	$result = $statement->execute();
+	if ($result)
+	{
+		//$_SESSION['username'] = $username;
+		//$_SESSION['employee_name'] = $name;
+		header("Location: homepage.php");
+		die(); 
+	}
+	else
+	{
+		$badLogin = true;
+	}
+}
+else
+{
+	header("Location: homepage.php");
+	die(); 
+}
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,7 +55,7 @@
 
 <h1>Sign up for new account</h1>
 
-<form id="signUpForm" action="createAccount.php" method="POST">
+<form id="signUpForm" action="signUp.php" method="POST">
 
 	<input type="text" id="employee_id" name="employee_id" placeholder="Employee ID">
 	<label for="employee_id">Employee ID</label>
