850f51a33206cdd9002fe11ba5811515a4c5e5a4
Jeff <rubikscubedude@gmail.com>
2021-07-03 13:30:13 -0700

    trying to fix the add an employee page

--- web/signUp.php
+++ web/signUp.php
@@ -1,3 +1,34 @@
+<?php
+
+if (isset($_POST['employee_id']) && isset($_POST['employee_password']) && isset($_POST['employee_name'])
+{
+	$id = $_POST['employee_id'];
+	$password = $_POST['employee_password'];
+	$name = $_POST['employee_name'];
+	
+	require("dbConnect.php");
+	$db = get_db();
+	$query = 'INSERT INTO naf_employee VALUES (:id, :password, :name)';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':id', $id);
+	$statement->bindValue(':name', $name);
+	$statement->bindValue(':password', $password);
+	$statement->execute();
+	header("Location: homepage.php");
+	die(); 
+	
+}
+else
+{
+	header("Location: homepage.php");
+	die(); 
+}
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,10 +45,10 @@
 
 <h1>Sign up for new account</h1>
 
-<form id="mainForm" action="createAccount.php" method="POST">
+<form id="signUpForm" action="signUp.php" method="POST">
 
-	<input type="text" id="employee_ID" name="employee_ID" placeholder="Employee ID">
-	<label for="employee_ID">Employee ID</label>
+	<input type="text" id="employee_id" name="employee_id" placeholder="Employee ID">
+	<label for="employee_id">Employee ID</label>
 	<br /><br />
 
 	<input type="password" id="employee_password" name="employee_password" placeholder="Password"></input>
