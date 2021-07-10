b7d702a5d0a16d501303374a0d78207f2470362b
Jeff <rubikscubedude@gmail.com>
2021-07-10 14:03:50 -0700

    hopefully getting the login checker to work

--- web/homepage.php
+++ web/homepage.php
@@ -19,11 +19,20 @@ if (isset($_POST['employee_id']) && isset($_POST['employee_password']))
 	$result = $statement->execute();
 	if ($result)
 	{
-		$_SESSION[':username'] = $username;
-		//$_SESSION['employee_name'] = $name;
-		header("Location: clock-in-out-page.php");
-		die(); 
+		$row = $statement->fetch();
+		$hashedPasswordFromDB = $row['employee_password'];
+		if (password_verify($password, $hashedPasswordFromDB))
+		{
+			$_SESSION['username'] = $username;
+			header("Location: clock-in-out-page.php");
+			die();
+		}
+		else
+		{
+			$badLogin = true;
+		}
 	}
+		
 	else
 	{
 		$badLogin = true;
