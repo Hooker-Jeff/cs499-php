9ca21517feaabd7ed795872deee5d0f4739cbab0
Jeff <rubikscubedude@gmail.com>
2021-07-15 16:29:51 -0700

    hopefully getting the login checker to work this time

--- web/createAccount.php
+++ web/createAccount.php
@@ -21,7 +21,7 @@ try{
 	$statement->bindValue(':id', $id);
 	$statement->bindValue(':password', $password);
 	$statement->bindValue(':name', $name);
-	statement->execute();
+	$statement->execute();
 	
 }
 /*catch (Exception $ex)
