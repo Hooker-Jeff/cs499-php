06d4d3d765c296e752f769bb49474cecd7e4353b
Jeff <rubikscubedude@gmail.com>
2021-07-15 19:08:06 -0700

    trying a new way to verify passwords

--- web/homepage.php
+++ web/homepage.php
@@ -20,10 +20,13 @@ if (isset($_POST['employee_id']) && isset($_POST['employee_password']))
 	if ($result)
 	{
 		//$row = $statement->fetch();
-		$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-		if (password_verify($password, $hashedPassword))
+		//$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+		//if (password_verify($password, $hashedPassword))
+			
+		$check = mysql_query("SELECT :username, :password FROM naf_employee WHERE employee_id = '".$username."' && employee_password = '".$password."'");
+		if ($check && mysql_num_rows($check) > 0)
 		{
-			$_SESSION['username'] = $username;
+			$_SESSION[':username'] = $username;
 			header("Location: clock-in-out-page.php");
 			die();
 		}
